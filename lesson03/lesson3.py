6c3e775cccf559e463bab06d118804f085e8c630
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-11 11:54:19 -0500

    fence

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,11 @@
-# Lesson 3
+# import statement (from module import functions)
+from math import sqrt, floor
+
+# input
+tiles = int(input("Enter the number of tiles: "))
+
+# processing
+largestSide = floor(sqrt(tiles))
+
+# output
+print(f"The largest square side possible is {largestSide}.")
--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,18 @@
-# Lesson 4
+# import statement 
+from math import ceil
+
+# input 
+section1 = input("Enter number of planks in section 1: ")
+section2 = input("Enter number of planks in section 2: ")
+section3 = input("Enter number of planks in section 3: ")
+
+# processing 
+cans = len(section1) + len(section2) + len(section3)
+boxes = ceil(cans / 12)
+leftOver = (boxes * 12) - cans
+cost = boxes * 14.95
+
+# output 
+print(f"Number of paint cans required: {cans}")
+print(f"Number of paint cans left over: {leftOver}")
+print(f"Total cost: ${cost}")
