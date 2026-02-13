6ebcd85b9e5a329ecabad30ebf4de1aa1c6c8093
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-13 08:58:46 -0500

    DUNGEONS AND DRAGONS RAHHHHH

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,12 @@
-# Lesson 7
+from random import randrange
+
+difficultyClass = int(input("Enter the difficulty class: "))
+
+# Random range: randrange(a, b) 
+# Random generated number is between a to b, including a, but not b
+D20 = randrange(1, 21)
+
+if D20 >= difficultyClass: 
+    print(f"The player rolled a {D20}. The action succeeds.")
+else: 
+    print(f"The player rolled a {D20}. The action fails.")
