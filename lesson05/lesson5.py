334d598b01d23bf83c8ca10cae6cb99dd3924a80
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-13 08:44:29 -0500

    cookies

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,22 @@
-# Lesson 5
+startingMoney = float(input("Enter the amount of starting money: "))
+cookiesSold = (input("Enter the cookies sold: "))
+
+bigCookies = cookiesSold.count("b")
+normalCookies = cookiesSold.count("c")
+
+# For each loop: for vairableName in listOrStringName 
+# for cookieType in cookiesSold: 
+#     if cookieType == "c":
+#         normalCookies += 1
+#     elif cookieType == "b":
+#         bigCookies += 1
+#     else: 
+#         print(f"{cookieType} is not a valid item.")
+
+totalCookies = bigCookies + normalCookies
+profit = (bigCookies * 1.25) + (normalCookies * 0.75)
+totalMoney = startingMoney + profit
+
+print(f"{totalCookies} were sold.")
+print(f"${profit} was made.")
+print(f"Total money is now ${totalMoney}.")
