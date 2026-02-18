f014967042347e0423054d5bc408a23810979344
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-18 16:17:41 -0500

    rock, paper, scissors

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,34 @@
-# Lesson 9
+# import statement 
+from random import choice 
+
+# input
+invalid = True
+player = ""
+
+while invalid: 
+    player = input("Enter rock, paper, or scissors: ")
+    if player in {"rock", "paper", "scissors"}:
+        invalid = False 
+
+# processing 
+# choice() randomly assigns a value from the list 
+ai = choice(["rock", "paper", "scissors"])
+
+if player == ai:
+    print("Tied Game")
+else: 
+    if player == "rock":
+        if ai == "paper":
+            print("AI Won")
+        else:
+            print("Player Won")
+    elif player == "paper":
+        if ai == "scissors":
+            print("AI Won")
+        else:
+            print("Player Won")
+    else: 
+        if ai == "rock":
+            print("AI Won")
+        else:
+            print("Player Won")
