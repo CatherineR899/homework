d495aec43b524be2573c9faf089616e9e43ceb6a
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-18 22:14:08 -0500

    factorial

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,16 @@
-# Lesson 17
+# input
+num = int(input("Enter a number: "))
+product = 1
+
+# multiplier = 2
+# while multiplier <= num:
+#     product *= multiplier
+#     multiplier += 1
+
+# for multiplier in range(num, 0, -1):
+#     product *= multiplier
+
+for multiplier in range(2, num + 1):
+    product *= multiplier
+
+print(f"The factorial of {num} is {product}.")
