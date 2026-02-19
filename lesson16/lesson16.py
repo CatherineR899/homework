bf477367b3ee5fd2881def2ea1bf990fba937bbb
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-18 22:03:42 -0500

    FizzBuzz

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,3 +1,13 @@
+# range() is a generator function that creates iterable integers in a given parameter 
+
+# range(x) generates numbers 0 up to x, not including x
+# range(a, b) generates numbers a up to b, not including b
+# range(a, b, c) generates numbers a up to b, not including b, but the numbers increase by c
+#   e.g. range(1, 10, 2) generates 1, 3, 5, 7, 9
+#   e.g. range(1-, 0, -1) generates 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 
+
+# for loops are used to look through values in an iterable object
+
 for num in range(1,51):
     if num % 3 == 0 and num % 5 == 0: 
         print("FizzBuzz")
