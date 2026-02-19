96a72ecbbaed5c0cde59a366798bf8be5d99a415
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-18 21:38:52 -0500

    feb 18

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,16 @@
-# Lesson 13
+# input
+month = int(input("Enter the month as a number: "))
+day = int(input("Enter the day: "))
+
+# processing + output 
+if month < 2:
+    print("Before")
+elif month > 2:
+    print("After")
+else: 
+    if day < 18:
+        print("Before")
+    elif day > 18:
+        print("After")
+    else:
+        print("Special")
