b3301ddfb22a281e987f4910ed098fa3187fa4e7
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-18 21:49:11 -0500

    loop

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,11 @@
-# Lesson 15
+# 2 nested loops (a loop within a loop) MAX 
+
+# while loop: can do infinite interations, is generally slower 
+#   while condition: 
+#       code block
+
+# for loop: does finite iterations, is generally faster 
+#   for vairableName in iterableObjectName:
+#       code block 
+#   for _ in range(n):
+#       code block 
