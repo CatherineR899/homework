e5ff0ac3b51d6831768866df1bcbd319c0e44b7c
CatherineR899 <catherine.ren2334@gmail.com>
2026-03-19 12:53:03 -0400

    8658

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,25 @@
-# Lesson 20
+# # my try 
+# totalSum = 0
+# for num in range(2, 10000):
+#     factors = []
+#     for divider in range(1, num):
+#         if num % divider == 0:
+#             factors.append(divider)
+#     factorSum = 0
+#     for factor in factors:
+#         factorSum += factor
+#     if factorSum == num:
+#         totalSum += num
+# print(totalSum)
+
+
+# mr park's solution 
+total_sum = 0
+for num in range(1, 10000):
+    factor_sum = 0
+    for divider in range(1, num):
+        if num % divider == 0:
+            factor_sum += divider
+    if factor_sum == num:
+        total_sum += num
+print(total_sum)
