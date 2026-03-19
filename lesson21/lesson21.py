24c947729de0c72c3b9d6e0df5f373c127485641
CatherineR899 <catherine.ren2334@gmail.com>
2026-03-19 13:09:12 -0400

    factors

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,28 @@
-# Lesson 21
+# my try
+N = int(input())
+greatestNum = 0
+greatestNumOfFactors = 0
+for num in range(1, N + 1):
+    numOfFactors = 0
+    for factor in range(1, num + 1):
+        if num % factor == 0:
+            numOfFactors += 1
+    if numOfFactors > greatestNumOfFactors: 
+        greatestNumOfFactors = numOfFactors
+        greatestNum = num
+print(greatestNum)
+
+
+# # mr. park's solution 
+# uppper_limit = int(input())
+# max_count = 0
+# result_num = 0
+# for num in range(1, uppper_limit + 1):
+#     total_factors = 0
+#     for divider in range(1, num + 1):
+#         if num % divider == 0:
+#             total_factors += 1
+#     if total_factors > max_count:
+#         max_count = total_factors
+#         result_num = num
+# print(f"{result_num} had the most factors, with {max_count} factors.")
