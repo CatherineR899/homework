6394f60978f1f97c1b0793c06bcb0ded9165ba0b
CatherineR899 <catherine.ren2334@gmail.com>
2026-04-20 19:02:53 -0400

    average

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,35 @@
-# Lesson 23
+# # my try 
+# total_sum = 0
+# num_inputs = 0
+# while True:
+#     user_input = input()
+#     if user_input.lower() == "exit":
+#         break
+#     elif int(user_input) >= 0 and int(user_input) <= 100:
+#         total_sum += int(user_input)
+#         num_inputs += 1
+# average = total_sum/num_inputs
+# print(average)
+# # what's wrong? cannot input anything besides numbers or exit, other words cause an error 
+
+# mr. park's
+loop = True 
+
+total_sum = 0
+counter = 0
+
+while loop:
+    user_input = input("Enter the mark or Exit to stop inputting marks. ")
+    if user_input.lower().capitalize() == "Exit":
+        loop = False
+        break
+    else:
+        mark = int(user_input)
+        if 0 <= mark and mark <= 100:
+            total_sum += mark
+            counter += 1
+        else:
+            print("Invalid input.")
+
+average = total_sum / counter
+print(f"Mark Average: {average}")
