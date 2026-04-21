9c060d6b96ca295d0a189e50d85beac15b45dca9
CatherineR899 <catherine.ren2334@gmail.com>
2026-04-20 23:46:19 -0400

    character

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,15 @@
-# Lesson 27
+# my try 
+def only_letters(word):
+    letters = ""
+    for character in word:
+        if character.lower() in "abcdefghijklmnopqrstuvwxyz":
+            letters += character
+    return letters
+
+# Mr. Park's 
+def string_cleaner(text):
+    result = ""
+    for character in text:
+        if character.isalpha():
+            result += character
+    return result
