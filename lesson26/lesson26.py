eceb1c68c8c37e5f4312b14ceea2c7845d3ecc86
CatherineR899 <catherine.ren2334@gmail.com>
2026-04-20 23:28:44 -0400

    factors

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,8 @@
-# Lesson 26
+def num_of_factors(N):
+    counter = 2
+    for factor in range(2, int(N * 0.5) + 1):
+        if N % factor == 0:
+            counter += 1
+    return counter
+
+print(num_of_factors(25))
